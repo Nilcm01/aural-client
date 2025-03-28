a90b9f614afdb9ffe0a769707bb53a2651a67537
Nilcm01 <nilcm01@outlook.es>
2025-03-28 01:23:24 +0100

    Chat: functional list of chats (no api)

--- aural-front/app/(tabs)/chats.tsx
+++ aural-front/app/(tabs)/chats.tsx
@@ -6,8 +6,29 @@ export default function ChatsScreen() {
 
     // List of all the chats of the current user
 
+    // TODO: get all chats of the current user
+    function getAllChats() {
+        // TODO: get all chats of the current user via API
+        // DEBUG: return a list of chats
+        return [
+            { id: 1, name: "Chat 1" },
+            { id: 2, name: "Chat 2" },
+            { id: 3, name: "Chat 3" },
+            { id: 4, name: "Chat 4" }
+        ];
+    }
+
+    const chats = getAllChats().map((chat) => {
+        const rute = `../components/chat?chatId=${chat.id}`;
+        return (
+            <Pressable onPress={() => router.push(rute as any)} style={{ backgroundColor: "#262626", padding: 20, borderRadius: 10, margin: 10, alignItems: "flex-start", justifyContent: "flex-start" }}>
+                <Text style={{ color: "white", fontSize: 16 }}>{chat.name}</Text>
+            </Pressable>
+        );
+    });
+
     return (
-        <View style={{ flex: 1, backgroundColor: "#000000" }}>
+        <View style={{ flex: 1, backgroundColor: "#000000", paddingTop: 100 }}>
             <View className="app-bar" style={{
                 height: 80, backgroundColor: "#262626",
                 alignItems: "center", top: 0, position: "absolute", width: "100%", display: "flex", flexDirection: "row", paddingHorizontal: 30, justifyContent: "center", zIndex: 10
@@ -16,9 +37,8 @@ export default function ChatsScreen() {
                 </Text>
             </View>
 
-            <Pressable onPress={() => router.push("../components/chat?chatId=1")} style={{ backgroundColor: "#262626", padding: 20, borderRadius: 10, margin: 10, alignItems: "flex-start", justifyContent: "flex-start", marginTop: 100 }}>
-                <Text style={{ color: "white", fontSize: 16 }}>Chat (debug)</Text>
-            </Pressable>
+            {/* List of chats */}
+            {chats}
         </View>
     );
 }
