ad1f052f28bb69afa54d87b7f28c96ee829ad4cc
Pau Dinares <paudinares@gmail.com>
2025-04-04 19:01:11 +0200

    Added a redirection to ProfileScreen in Appbar

--- aural-front/app/components/appBar.tsx
+++ aural-front/app/components/appBar.tsx
@@ -25,7 +25,7 @@ export default function AppBar() {
           color="white"
           onPress={() => {
             if (token) {
-              // TODO: Go to profile screen
+              router.push("/profileScreen"); // Navigate to profile screen
             } else {
               router.push("/loginScreen"); // Navigate to login screen
             }
