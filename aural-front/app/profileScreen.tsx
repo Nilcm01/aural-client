58a01aa5245e23d80cc4cf31cb6cfbbda6abdec3
Nilcm01 <nilcm01@outlook.es>
2025-05-13 12:23:40 +0200

    Moved the logout button from AppBar to ProfileScreen

--- aural-front/app/profileScreen.tsx
+++ aural-front/app/profileScreen.tsx
@@ -11,7 +11,7 @@ const API_URL = 'https://aural-454910.ew.r.appspot.com/api/items/';
 
 const ProfileScreen = () => {
   const router = useRouter();
-  const { token } = useToken();
+  const { token, logout } = useToken();
   const { showReproBar } = useReproBarVisibility();
   useFocusEffect(() => {
         showReproBar(false);
@@ -319,6 +319,9 @@ const ProfileScreen = () => {
             </TouchableOpacity>
             <TouchableOpacity style={styles.menuItem} onPress={openStatsModal}>
               <Text style={styles.menuText}>Stats</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.menuItem} onPress={ () => { logout(); closeOptionsMenu(); } }>
+              <Text style={[styles.menuText, {color: "#F05858"}]}>Sign out</Text>
             </TouchableOpacity>
           </View>
         </TouchableOpacity>
